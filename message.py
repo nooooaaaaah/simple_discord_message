c2751c9c038615150121958fbc13c4fc94046839
Noah <nspielman@live.com>
2023-03-25 16:10:45 -0600

    simple message bot

---
+++ message.py
@@ -0,0 +1,15 @@
+import discord
+import asyncio
+
+
+import discord
+import asyncio
+
+
+async def send_direct_message(token, user_id, message):
+    intents = discord.Intents.default()
+    async with discord.Client(intents=intents) as client:
+        await client.login(token)
+        user = await client.fetch_user(user_id)
+        await user.send(message)
+        # print(f"Direct message sent to user {user_id}: {message}")
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Simple Message Bot
+
+This bot is designed to send a custom direct message to a user on discord.
+
+## Example
+
+```python
+import asyncio
+from message import send_direct_message
+
+
+async def my_script():
+    # Your script code goes here
+    await send_direct_message(TOKEN, USER_ID, "Hello, World!")
+
+
+async def main():
+    await my_script()
+
+asyncio.run(main())
+
+```
